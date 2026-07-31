5680082ee98aa32de97c69b9c7ed3168ca3ff87e
wtfditoh <155072086+wtfditoh@users.noreply.github.com>
2026-07-30 23:11:57 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -21,17 +21,4 @@ messaging.onBackgroundMessage((payload) => {
     '[firebase-messaging-sw.js] Mensagem recebida:',
     payload
   );
-
-  const notificationTitle = payload.notification.title;
-
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/icon-192.png",
-    badge: "/icon-192.png"
-  };
-
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
 });
