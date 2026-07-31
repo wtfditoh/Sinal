84681a4774780c7735a97cac3729c30c49db5a27
wtfditoh <155072086+wtfditoh@users.noreply.github.com>
2026-07-30 23:00:05 -0300

    Update enviar-notificacao.js

--- netlify/functions/enviar-notificacao.js
+++ netlify/functions/enviar-notificacao.js
@@ -1,18 +1,14 @@
 const admin = require("firebase-admin");
 
 
-if (!admin.apps.length) {
-
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-  );
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 
 const db = admin.firestore();
