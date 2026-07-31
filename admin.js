e5368e896dc58eb328c764bea1de7b1d1b46f7a0
wtfditoh <155072086+wtfditoh@users.noreply.github.com>
2026-07-30 21:44:39 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,11 +4,60 @@ import { exigirLogin } from "./auth.js";
 exigirLogin(async (usuario)=>{
 
 
-console.log("Usuário:", usuario);
-
-
 document.getElementById("usuario").innerHTML =
 "Usuário conectado";
+
+
+document.getElementById("enviar")
+.addEventListener("click", async ()=>{
+
+
+const titulo = document.getElementById("titulo").value;
+
+const mensagem = document.getElementById("mensagem").value;
+
+
+if(!titulo || !mensagem){
+
+alert("Preencha título e mensagem");
+
+return;
+
+}
+
+
+
+const resposta = await fetch(
+"/.netlify/functions/enviar-notificacao",
+{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body: JSON.stringify({
+
+titulo,
+mensagem
+
+})
+
+});
+
+
+const resultado = await resposta.json();
+
+
+console.log(resultado);
+
+
+alert("Notificação enviada!");
+
+
+
+});
 
 
 });
